cbde52c105af740893f16e8cf853dd654c3e3cb7
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-04-03 15:54:27 -0300

    Correcao de bugs, Eventos: adicionar, onChange, consulta e exclusao

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,29 +3,30 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
-    output:{
+    output: {
         path: __dirname + '/public',
         filename: './app.js'
     },
+
     devServer: {
-        port: 8090,
-        contentBase: './public'
+        port: 8080,
+        contentBase: './public',
     },
-    resolve:{
+    resolve: {
         extensions: ['', '.js', '.jsx'],
-        alias:{
+        alias: {
             modules: __dirname + '/node_modules'
-        }
+    }
     },
     plugins: [
         new ExtractTextPlugin('app.css')
     ],
-    module:{
-        loaders:[{
+    module: {
+        loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
-            query:{
+            query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
